89707512f7c26c631b1913512e0b70115d40f1bf
ginjardev <arthurshur@gmail.com>
2024-05-07 13:44:19 -0700

    task 3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,5 @@
+# Set up client SSH configuration file so that you can connect to a server without typing a password
+
 exec { 'echo':
   path    => 'usr/bin:/bin',
   command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
